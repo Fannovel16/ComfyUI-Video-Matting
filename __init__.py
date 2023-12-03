078b5c4c1d995c6737a5cf754434bc9b9cea3bc6
Hacker 17082006 <tai17082006.tk@gmail.com>
2023-12-03 11:47:59 +0700

    Final

--- __init__.py
+++ __init__.py
@@ -4,6 +4,7 @@ import os, yaml
 from torch.hub import download_url_to_file, get_dir
 from urllib.parse import urlparse
 from comfy.model_management import soft_empty_cache, get_torch_device
+from PIL import ImageColor
 
 config_path = os.path.join(os.path.dirname(__file__), "./config.yaml")
 if os.path.exists(config_path):
@@ -56,8 +57,9 @@ class RobustVideoMatting:
         return {
             "required": {
                 "video_frames": ("IMAGE",), 
-                "backbone": (["mobilenetv3", "resnet50"], {"default": "mobilenetv3"}),
+                "backbone": (["mobilenetv3", "resnet50"], {"default": "resnet50"}),
                 "fp16": ("BOOLEAN", {"default": True}),
+                "bg_color": ("STRING", {"default": "green"}),
                 "batch_size": ("INT", {"min": 1, "max": 64, "default": 4})
             }
         }
@@ -66,13 +68,16 @@ class RobustVideoMatting:
     FUNCTION = "matting"
     CATEGORY = "Video Matting/Robust Video Matting"
 
-    def matting(self, video_frames, backbone, fp16, batch_size):
+    def matting(self, video_frames, backbone, fp16, bg_color, batch_size):
         model_path = load_file_from_url(download_url_template.format(backbone=backbone, dtype="fp16" if fp16 else "fp32"), model_dir=CKPTS_PATH)
         model = torch.jit.load(model_path, map_location="cpu")
         model.to(device)
         video_frames = rearrange(video_frames, "n h w c -> n c h w")
+        bg_color = torch.Tensor(ImageColor.getrgb(bg_color)[:3]).to(device).float() / 255.
+        bg_color = repeat(bg_color, "c -> n c 1 1", n=batch_size)
         if fp16:
             model.half()
+            bg_color.half()
         model = torch.jit.freeze(model)
         orig_num_frames = video_frames.shape[0]
         pad_frames = repeat(video_frames[-1:], "1 c h w -> n c h w", n=batch_size - (orig_num_frames % batch_size))
@@ -83,9 +88,10 @@ class RobustVideoMatting:
             if fp16:
                 input = input.half()
             fgr, pha, *rec = model(input, *rec, auto_downsample_ratio(*video_frames.shape[2:]))
-            fgr = fgr * pha.gt(0)
+            mask = pha.gt(0)
+            fgr = fgr * mask + bg_color * ~mask
             fgrs.append(fgr.float())
-            masks.append(pha.float())
+            masks.append(mask.float())
         fgrs = rearrange(torch.cat(fgrs, dim=0), "n c h w -> n h w c")[:orig_num_frames].cpu().detach()
         masks = torch.cat(masks, dim=0)[:orig_num_frames].cpu().detach()
         soft_empty_cache()
